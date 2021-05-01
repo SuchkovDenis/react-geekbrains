7d7e186c951d19619c7823b5468b1671b043b8a3
suchkovdenis <ny1828@yandex.ru>
2021-05-01 12:28:20 +0300

    lesson1 webpack and react init

---
+++ static_src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MessageComponent = ( {text} ) => <div>{text}</div>;
+
+class MessageField extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages : [],
+            count : 1
+        }
+    }
+
+    addMessage = () => {
+        this.setState(prevState => {
+            return { 
+                messages : [...prevState.messages, 'Сообщение ' + prevState.count],
+                count : prevState.count + 1
+            };
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.addMessage}>Жмакни</button>
+                {this.state.messages.map(message => <MessageComponent text={ message } />)}
+            </div>
+        );
+    }
+ };
+
+ReactDOM.render(
+    <MessageField/>,
+    document.getElementById('root'),
+);
